2bf487ecb4f0ecf749eac1feaaa4c1ba1aff3faf
gadielwiggy <gadielwiggy@gmail.com>
2014-10-28 20:54:38 -0400

    Update PartTimeFaculty.java

--- src/PartTimeFaculty.java
+++ src/PartTimeFaculty.java
@@ -1,17 +1,51 @@
 
 public class PartTimeFaculty extends FacultyMember{
-private int hours;
+
+	private int hours;
 	
-	public PartTimeFaculty(){
-		super();
-		hours = 0;
+	
+
+
+	private double hourlyRate;
+	
+
+
+	private int bonus;
+	public PartTimeFaculty(String id, String name, int hours, double hourlyRate, int numCourses){
+		
+		super(id, name, hours*hourlyRate, numCourses);
+		bonus=0;
+		for(int i=0;i<numCourses;i++){
+			if(getStudentsPerClass(i)>60)
+				bonus+=1000;
+			if(getStudentsPerClass(i)>40 && getStudentsPerClass(0)<60)
+				bonus+=500;
+		}
+		setMonthlyPay(hours*hourlyRate+bonus);
+		this.hours=hours;
+		this.hourlyRate=hourlyRate;
 	}
 	
-	public void setHours(int hours){
+	
+	
+	public double getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+	
+	public void setHourlyRate(double hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+
+	public void setHours(int hours) {
 		this.hours = hours;
 	}
 	
-	public int getHours(){
-		return hours;
+	public String paymentStub(){
+		return "name:"+getName()+"\nid:"+getID()+"\npayment:"+getMonthlyPay()+"\nhours worked"+hours+"\nhouly rate:"+hourlyRate;
 	}
+	
 }
