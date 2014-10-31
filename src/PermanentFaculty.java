36781de3eb3bacba0870410d818c0a9760b217c3
gadielwiggy <gadielwiggy@gmail.com>
2014-10-30 23:38:00 -0400

    Update PermanentFaculty.java

--- src/PermanentFaculty.java
+++ src/PermanentFaculty.java
@@ -1,7 +1,10 @@
+import java.io.Serializable;
 
-public class PermanentFaculty extends FacultyMember {
+public class PermanentFaculty extends FacultyMember implements Serializable {
+	
+		public PermanentFaculty(String id, String name, double monthlyPay, int numCourses,String[] classNames, int[] studentsPerClass){
+			super(id, name, monthlyPay, numCourses, classNames, studentsPerClass);
+		}
+	
 
-	public PermanentFaculty(String id, String name, double monthlyPay, int numCourses){
-		super(id, name, monthlyPay, numCourses);
-	}
 }
