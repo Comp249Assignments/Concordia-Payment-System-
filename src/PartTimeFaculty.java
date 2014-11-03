f516c1568cce7d1088e6227632f70e084d9ff53f
gadielwiggy <gadielwiggy@gmail.com>
2014-11-02 20:20:46 -0500

    Update PartTimeFaculty.java

--- src/PartTimeFaculty.java
+++ src/PartTimeFaculty.java
@@ -47,4 +47,7 @@ public class PartTimeFaculty extends FacultyMember implements Serializable{
 		return (super.toString+"\nHours: "+hours+"\nHourly wage: "+hourlyRate+"\nBonus: "+bonus);
 	}
 	
+	public int getMonthsLeft(){
+		return this.monthsLeft;
+	}
 }
