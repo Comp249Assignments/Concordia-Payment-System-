4b01007a8f8925e547eb3f614dc33316b3279c25
SeanMarcoux <seanmarcoux@hotmail.com>
2014-11-09 12:21:42 -0500

    Added constructor for undergrad TA
    For the base hourly rate for undergrad tas

--- src/UnderGradTA.java
+++ src/UnderGradTA.java
@@ -3,6 +3,10 @@ public class UnderGradTA extends TA{
 	public UnderGradTA(){
 		super();
 	}
+	//Constructor for the TA who will hold the base hourly salary for undergrad TAs
+	public UnderGradTA(double hourlyPay){
+		super(0, "", hourlyPay, 1000000, 1);
+	}
 	public UnderGradTA(int id, String name, double hourlyPay, int monthsUntilGraduation, int hours){
 		super(id, name, hourlyPay, monthsUntilGraduation, hours);
 	}
